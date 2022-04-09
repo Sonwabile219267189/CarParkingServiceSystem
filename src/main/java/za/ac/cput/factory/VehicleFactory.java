1a44d611a448de9c8b11f2518eed022cc5545853
ZintleZothe <216130565@mycput.ac.za>
2022-04-09 14:09:01 +0200

    first commit: added Role and Vehicle

---
+++ src/main/java/za/ac/cput/factory/VehicleFactory.java
@@ -0,0 +1,24 @@
+package za.ac.cput.factory;
+/*VehicleFactory.java
+  Factory class for the Vehicle
+  Author: Zintle Zothe (216130565)
+  Date: 07 April 2022
+ */
+import za.ac.cput.entity.Vehicle;
+import za.ac.cput.helper.GenerateID;
+
+
+public class VehicleFactory {
+    public static Vehicle createVehicle(String vehicleId, String vehiclePlateNumber, String vehicleType, String vehicleModel, String vehicleColour){
+
+        String id= GenerateID.generateUUID();
+
+        Vehicle vehicle = new Vehicle.Builder().setVehicleId(id)
+                .setVehiclePlateNumber(vehiclePlateNumber)
+                .setVehicleType(vehicleType)
+                .setVehicleModel(vehicleModel)
+                .setVehicleColour(vehicleColour)
+                .build();
+        return vehicle;
+    }
+}

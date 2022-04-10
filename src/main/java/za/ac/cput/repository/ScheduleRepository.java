9d9e2fbecc833b49da0412e21d45cd94722c2dbc
Hlombekazi <209024666@mycput.ac.za>
2022-04-10 16:20:21 +0200

    Second commit , created payment and Schedule repository classes  , with their test classes.

---
+++ src/main/java/za/ac/cput/repository/ScheduleRepository.java
@@ -0,0 +1,77 @@
+package za.ac.cput.repository;
+/**
+ * PaymentRepository.java
+ * Interface class
+ * Author: Hlombekazi Mbelu (209024666)
+ * Date: 9 April 2022
+ */
+
+
+import za.ac.cput.entity.Campus;
+import za.ac.cput.entity.Payment;
+import za.ac.cput.entity.Schedule;
+
+import java.util.*;
+
+public class ScheduleRepository implements IScheduleRepository {
+
+    public static ScheduleRepository repository = null;
+    private List<Schedule> scheduleDB = null;
+    private String scheduleId;
+
+
+    private ScheduleRepository() {
+        scheduleDB = new ArrayList<>();
+
+    }
+
+    public static ScheduleRepository getRepository() {
+        if (repository == null) {
+            repository = new ScheduleRepository();
+        }
+        return repository;
+    }
+
+    @Override
+    public List<Schedule> getAll() {
+        return scheduleDB;
+    }
+
+    @Override
+    public Schedule create(Schedule schedule) {
+        scheduleDB.add(schedule);
+        return null;
+    }
+
+    @Override
+    public Schedule read(String s) {
+        for(Schedule schedule :scheduleDB)
+            if(schedule.getScheduleID().equals(scheduleId)){
+                return schedule;
+
+            }
+        return null;
+    }
+
+    @Override
+    public Schedule update(Schedule schedule) {
+        Schedule oldSchedule = read(schedule.getScheduleID());
+        if(oldSchedule != null){
+            scheduleDB.remove(oldSchedule);
+            scheduleDB.add(schedule);
+            return schedule;
+        }
+        return null;
+    }
+
+    @Override
+    public  boolean delete(String s) {
+        Schedule scheduleDelete = read(scheduleId);
+        if(scheduleDelete == null){
+            return false;
+        }
+        scheduleDB.remove(scheduleDelete);
+        return true;
+    }
+
+}

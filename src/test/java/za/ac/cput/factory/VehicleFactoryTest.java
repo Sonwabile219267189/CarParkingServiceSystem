e7b3e69c957155edcaca81255bef9eec17a7b242
ZintleZothe <216130565@mycput.ac.za>
2022-08-06 22:19:12 +0200

    Factory classes and test edits

--- src/test/java/za/ac/cput/factory/VehicleFactoryTest.java
+++ src/test/java/za/ac/cput/factory/VehicleFactoryTest.java
@@ -14,10 +14,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class VehicleFactoryTest {
     @Test
+    //successful test with correct input
     public void createVehicle(){
-        Vehicle vehicle= VehicleFactory.createVehicle("CF234246","SUV",  "Ford ecosport","Diamond white");
+        Vehicle vehicle= VehicleFactory.createVehicle("CF234246",  "Ford eco-sport, SUV","Diamond white");
         assertNotNull(vehicle);
         System.out.println(vehicle);
+
+    }
+
+    @Test
+        //Incorrect string input to test the isEmptyOrNull method
+    void CreateVehicleWithAnError(){
+        Exception exception=assertThrows(IllegalArgumentException.class, ()->
+                VehicleFactory.createVehicle("","Ford eco-sport, SUV","Diamond white"));
+        System.out.println( exception.getMessage()+": vehicleNumberplate");
 
     }
 }

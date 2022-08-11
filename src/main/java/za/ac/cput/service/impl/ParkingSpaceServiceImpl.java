67947ae72ae3c530cf2bbf245faa685d56480f14
Sonwabile <219267189@mycput.ac.za>
2022-08-11 21:11:44 +0100

    Services

---
+++ src/main/java/za/ac/cput/service/impl/ParkingSpaceServiceImpl.java
@@ -0,0 +1,32 @@
+/**
+ * ParkingSpaceServiveImpl.java
+ * Implementation class for ParkingSpaceService.java
+ * Author: Sonwabile Gxoyiya (219267189)
+ * Date: 11 August 2022
+ */
+package za.ac.cput.service.impl;
+
+import za.ac.cput.entity.ParkingSpace;
+import za.ac.cput.service.ParkingSpaceService;
+
+public class ParkingSpaceServiceImpl implements ParkingSpaceService {
+    @Override
+    public ParkingSpace create(ParkingSpace parkingSpace) {
+        return null;
+    }
+
+    @Override
+    public ParkingSpace read(String s) {
+        return null;
+    }
+
+    @Override
+    public ParkingSpace update(ParkingSpace parkingSpace) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(String s) {
+        return false;
+    }
+}

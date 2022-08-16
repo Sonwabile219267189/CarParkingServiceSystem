d19872e548e17afaaf5c6613917d58055412a88d
Sonwabile <219267189@mycput.ac.za>
2022-08-16 15:38:59 +0100

    Added Controller package

---
+++ src/main/java/za/ac/cput/controller/ParkingSpaceController.java
@@ -0,0 +1,10 @@
+/**
+ * ParkingSpaceController.java
+ * Controller class for ParkingSpace.java
+ * Author: Sonwabile Gxoyiya (219267189)
+ * Date: 16 August 2022
+ */
+package za.ac.cput.controller;
+
+public class ParkingSpaceController {
+}
---
+++ src/test/java/za/ac/cput/controller/ParkingSpaceControllerTest.java
@@ -0,0 +1,13 @@
+/**
+ * ParkingSpaceControllerTest.java
+ * Test class for ParkingSpaceController.java
+ * Author: Sonwabile Gxoyiya (219267189)
+ * Date: 16 August 2022
+ */
+package za.ac.cput.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParkingSpaceControllerTest {
+
+}
